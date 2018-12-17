8a08fc3b8e65cc73c2429c9f37e8b90c0a09d33b
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-17 12:16:48 +0530

    added framework

---
+++ FOLDER/DocumentSelfieFramework.framework/Headers/DataModalClass.h
@@ -0,0 +1,19 @@
+//
+//  DataModalClass.h
+//  DocumentSelfieFramework
+//
+//  Created by IDM014 on 17/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DataModalClass : NSObject
+
+
+-(void)showMessage;
+
+@end
+
+
---
+++ FOLDER/DocumentSelfieFramework.framework/Headers/DocumentSelfieFramework.h
@@ -0,0 +1,19 @@
+//
+//  DocumentSelfieFramework.h
+//  DocumentSelfieFramework
+//
+//  Created by IDM014 on 17/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <DocumentSelfieFramework/DataModalClass.h>
+//! Project version number for DocumentSelfieFramework.
+FOUNDATION_EXPORT double DocumentSelfieFrameworkVersionNumber;
+
+//! Project version string for DocumentSelfieFramework.
+FOUNDATION_EXPORT const unsigned char DocumentSelfieFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentSelfieFramework/PublicHeader.h>
+
+
